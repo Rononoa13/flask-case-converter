7de26634067a50a6e759514ca04511ca14b5caee
Rononoa13 <sumitluitel42@gmail.com>
2022-12-13 21:00:33 +0545

    Fix routes

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template('home.html')
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -9,12 +13,12 @@ def home():
         replace_text = request.form.get('replace-text')
         note = request.form.get('user-note')
 
-        print(f"{find_char}")
-        print(f"{replace_text}")
-        print(f"{note}")
+        # print(f"{find_char}")
+        # print(f"{replace_text}")
+        # print(f"{note}")
 
         x = note.replace(find_char, replace_text)
-        print(f'this is changed text -> {x}')
+        # print(f'this is changed text -> {x}')
     return render_template('home.html', x=x)
 
 
