631dfd9e158cc56538912802543c0de75d8eccab
Sumit Luitel <sumit.luitel@abacusinsights.com>
2024-01-03 15:17:43 +0545

    Add '/json_converter' route and logic to prettify valid json format string

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ def index():
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+
     if request.method == 'POST':
         find_char = request.form.get('find-char')
         replace_text = request.form.get('replace-text')
@@ -17,6 +19,31 @@ def home():
 
     return render_template('home.html', user_text=user_text)
 
+@app.route("/json_converter", methods=['GET', 'POST'])
+def get_pretty_json():
+
+    if request.method == 'POST':
+
+        get_user_json = request.form.get('json-input')
+        if "'" in get_user_json:
+            get_user_json = get_user_json.replace("'", "\"")
+        json_data = json.loads(get_user_json)
+        print_json = json.dumps(json_data, indent=4)
+
+        # json_strings = get_user_json.split('}, {')
+
+        # if len(json_strings) < 1:
+        #     json_strings[0] = json_strings[0] + '}'
+        #     json_strings[-1] = '{' + json_strings[-1]
+
+        # formatted_objects = []
+        # for json_data in json_strings:
+        #     formatted_objects.append(json.dumps(json.loads(json_data), indent=4))
+        # print_json = formatted_objects
+
+        return render_template('json_formatter.html', pretty_json=print_json)
+
+    return render_template('json_formatter.html')
 
 if __name__== '__main__':
     app.run(debug=True)
